df543754cb391dc8d07cf4afa469abd6ab4478b8
Rodrigo Ribeiro (Keyser) <keyser_br@hotmail.com>
2023-01-03 00:15:29 -0300

    definido lista de usuarios e niveis

--- resources/js/Pages/Dashboard/list-users.vue
+++ resources/js/Pages/Dashboard/list-users.vue
@@ -1,41 +1,149 @@
 <template>
 
-    <Head title="Lista de usuários"></Head>
+  <Head title="Lista de usuários"></Head>
 
-    <v-card
-    class="mx-auto"
-    variant="flat"
-  >
-    
-    <v-list lines="two">
-      
 
-      <v-list-item
-        v-for="user in users"
-        :key="user.id"
-        :title="user.name"
-        :subtitle="user.email"
-      >
-        <template v-slot:prepend>
-          <v-avatar color="grey-lighten-1">
-            <v-icon color="white">mdi-account</v-icon>
-          </v-avatar>
-        </template>
-        
-        <template v-slot:append>
-          <v-btn
-            color="primary"
-            icon="md:edit"
-            variant="text"
-          ></v-btn>
-          
-        </template>
-        <v-divider ></v-divider>
-      </v-list-item>   
-      
-    </v-list>
-  </v-card>
+  <v-tabs v-model="tab" fixed-tabs>
+    <v-tab value="Admins">Admins</v-tab>
+    <v-tab value="Colaboradores">Colaboradores</v-tab>
+    <v-tab value="Supervisores">Supervisores</v-tab>
+    <v-tab value="Coordenadores">Coordenadores</v-tab>
+    <v-tab value="Gerentes">Gerentes</v-tab>
+    <v-tab value="Lojas">Lojas</v-tab>
 
+  </v-tabs>
+  <v-window v-model="tab">
+    <v-window-item value="Admins">
+      <v-card class="mx-auto" variant="flat">
+        <v-list lines="two">
+          <v-list-item v-for="user in users" :key="user.id" :title="user.name" :subtitle="user.email">
+            <template v-slot:prepend>
+              <v-avatar color="grey-lighten-1">
+                <v-icon color="white">mdi-account</v-icon>
+              </v-avatar>
+            </template>
+
+            <template v-slot:append>
+              <v-btn color="primary" icon="md:edit" variant="text"></v-btn>
+
+            </template>
+            <v-divider></v-divider>
+          </v-list-item>
+
+        </v-list>
+      </v-card>
+    </v-window-item>
+  </v-window>
+  <v-window v-model="tab">
+    <v-window-item value="Colaboradores">
+      <v-card class="mx-auto" variant="flat">
+        <v-list lines="two">
+          <v-list-item v-for="user in users" :key="user.id" :title="user.name" :subtitle="user.email">
+            <template v-slot:prepend>
+              <v-avatar color="grey-lighten-1">
+                <v-icon color="white">mdi-account</v-icon>
+              </v-avatar>
+            </template>
+
+            <template v-slot:append>
+              <v-btn color="primary" icon="md:edit" variant="text"></v-btn>
+
+            </template>
+            <v-divider></v-divider>
+          </v-list-item>
+
+        </v-list>
+      </v-card>
+    </v-window-item>
+  </v-window>
+  <v-window v-model="tab">
+    <v-window-item value="Supervisores">
+      <v-card class="mx-auto" variant="flat">
+        <v-list lines="two">
+          <v-list-item v-for="user in users" :key="user.id" :title="user.name" :subtitle="user.email">
+            <template v-slot:prepend>
+              <v-avatar color="grey-lighten-1">
+                <v-icon color="white">mdi-account</v-icon>
+              </v-avatar>
+            </template>
+
+            <template v-slot:append>
+              <v-btn color="primary" icon="md:edit" variant="text"></v-btn>
+
+            </template>
+            <v-divider></v-divider>
+          </v-list-item>
+
+        </v-list>
+      </v-card>
+    </v-window-item>
+  </v-window>
+  <v-window v-model="tab">
+    <v-window-item value="Coordenadores">
+      <v-card class="mx-auto" variant="flat">
+        <v-list lines="two">
+          <v-list-item v-for="user in users" :key="user.id" :title="user.name" :subtitle="user.email">
+            <template v-slot:prepend>
+              <v-avatar color="grey-lighten-1">
+                <v-icon color="white">mdi-account</v-icon>
+              </v-avatar>
+            </template>
+
+            <template v-slot:append>
+              <v-btn color="primary" icon="md:edit" variant="text"></v-btn>
+
+            </template>
+            <v-divider></v-divider>
+          </v-list-item>
+
+        </v-list>
+      </v-card>
+    </v-window-item>
+  </v-window>
+  <v-window v-model="tab">
+    <v-window-item value="Gerentes">
+      <v-card class="mx-auto" variant="flat">
+        <v-list lines="two">
+          <v-list-item v-for="user in users" :key="user.id" :title="user.name" :subtitle="user.email">
+            <template v-slot:prepend>
+              <v-avatar color="grey-lighten-1">
+                <v-icon color="white">mdi-account</v-icon>
+              </v-avatar>
+            </template>
+
+            <template v-slot:append>
+              <v-btn color="primary" icon="md:edit" variant="text"></v-btn>
+
+            </template>
+            <v-divider></v-divider>
+          </v-list-item>
+
+        </v-list>
+      </v-card>
+    </v-window-item>
+  </v-window>
+  <v-window v-model="tab">
+    <v-window-item value="Lojas">
+      <v-card class="mx-auto" variant="flat">
+        <v-list lines="two">
+          <v-list-item v-for="user in users" :key="user.id" :title="user.name" :subtitle="user.email">
+            <template v-slot:prepend>
+              <v-avatar color="grey-lighten-1">
+                <v-icon color="white">mdi-account</v-icon>
+              </v-avatar>
+            </template>
+
+            <template v-slot:append>
+              <v-btn color="primary" icon="md:edit" variant="text"></v-btn>
+
+            </template>
+            <v-divider></v-divider>
+          </v-list-item>
+
+        </v-list>
+      </v-card>
+    </v-window-item>
+  </v-window>
 </template>
 
 
@@ -44,15 +152,20 @@ import layoutDashaboard from '../../Layout/layout-dashboard.vue'
 
 export default {
 
-    layout: layoutDashaboard
-
+  layout: layoutDashaboard,
+  data(){
+    return {
+      tab: null
+    }
+  }
 }
 </script>
 <script setup>
 defineProps({
-    users: Object,
+  users: Object,
 
 
 })
+
 
 </script>
